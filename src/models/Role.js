0df218fdcbb4ce392b7b001c947478e08792dc5f
Charlotte Model <cmodel@me.com>
2024-04-10 14:54:25 +0200

    adjusted indents in player.js and role.js

--- src/models/Role.js
+++ src/models/Role.js
@@ -1,9 +1,9 @@
 class Role {
-    constructor(data = {}) {
-      this.roleName = null;
-      Object.assign(this, data);
-    }
+  constructor(data = {}) {
+  this.roleName = null;
+  Object.assign(this, data);
   }
+}
     
-  export default Role;
+export default Role;
     
