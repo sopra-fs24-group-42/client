fc82f87da6dec6c035603f5f528be7f49b3ad4b6
Charlotte Model <cmodel@me.com>
2024-04-22 15:21:26 +0200

    closes #78: implemented a basic view and UI for the reveal of the night phase action

--- src/components/views/NightAction.tsx
+++ src/components/views/NightAction.tsx
@@ -155,8 +155,7 @@ const NightAction = () => {
           {playersInLobby.map((user: User) => (
             <li key={user.username}
               onClick={() => setSelected(user.username)}
-              className={`player container ${selected === user.username ? "selected" : ""}`
-            }
+              className={`player container ${selected === user.username ? "selected" : ""}`}
             >
               < LobbyMember user={user} />
             </li>
--- src/components/views/NightReveal.tsx
+++ src/components/views/NightReveal.tsx
@@ -6,7 +6,7 @@ import { api, handleError } from "helpers/api";
 import { Spinner } from "components/ui/Spinner";
 import {useNavigate, useLocation} from "react-router-dom";
 import { Button } from "components/ui/Button";
-import "styles/views/RoleReveal.scss";
+import "styles/views/NightReveal.scss";
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
 import { User } from "types";
@@ -26,7 +26,6 @@ const NightReveal = () => {
   const [messageReceived, setMessageReceived] = useState(null);
   var killedPlayer = null;
   const username = localStorage.getItem("user"); //fetching username from localstorage
-  const [role, setRole] = useState(null); 
   const [ready, setReady] = useState(false);
   let gameState = "REVEALNIGHT";
 
@@ -34,12 +33,11 @@ const NightReveal = () => {
 
   const findKilledPlayer = () => {
     console.log("Inside findKilledPlayer");
-    for(let i=0; i<messageReceived.players.length; i++) {
-        console.log("LENGTH: " + messageReceived.players.length);
+    for(let i=0; i<messageReceived.players.length; i++) { // iterating through list of players to check their isKilled field 
       let currentPlayer = messageReceived.players[i];
-      console.log("CURRENT PLAYER: " + currentPlayer);
-      console.log("isKilledField: " + messageReceived.playerMap[`${currentPlayer.username}`].isKilled);
-      if(messageReceived.playerMap[`${currentPlayer.username}`].isKilled){
+      //console.log("CURRENT PLAYER: " + currentPlayer);
+      //console.log("isKilledField: " + messageReceived.playerMap[`${currentPlayer.username}`].isKilled);
+      if(messageReceived.playerMap[`${currentPlayer.username}`].isKilled){ 
         killedPlayer = currentPlayer;
       }
     }
@@ -50,7 +48,7 @@ const NightReveal = () => {
 
   const connect = async () => {
     return new Promise((resolve, reject) => {
-        console.log("INSIDE CONNECT");
+      console.log("INSIDE CONNECT");
       const socket = new SockJS(baseURL+"/game"); // creating a new SockJS object (essentially a websocket object)
       stompClient = over(socket); // specifying that it's a special type of websocket connection (i.e. using sockJS)
       stompClient.connect({}, function (frame) { // connecting to server websocket: instructions inside "function" will only be executed once we get something (i.e. a connect frame back from the server). Parameter "frame" is what we get from the server. 
@@ -75,11 +73,7 @@ const NightReveal = () => {
         console.log("I AM STILL SUBSCRIBED AND RECEIVED A MESSAGE");
         // all of this only gets executed when message is received
         console.log("this is the message: " + JSON.stringify(message));
-        //localStorage.setItem("lobby", message.body);
         setMessageReceived(JSON.parse(message.body));
-        //findKilledPlayer();
-        //console.log("THIS IS THE KILLED PLAYER " + killedPlayer);
-        //setRole(JSON.parse(message.body).playerMap[`${username}`].roleName);
         resolve(subscription);
       });
     }); 
@@ -88,7 +82,7 @@ const NightReveal = () => {
   useEffect(() => { // This is executed once upon mounting of roleReveal --> establishes ws connection & subscribes
     console.log("INSIDE USEEFFECT");
     if(!connection) {  
-    const connectAndSubscribe = async () => { 
+      const connectAndSubscribe = async () => { 
         try {
           await connect();
           subscription = await subscribe(`/topic/lobby/${lobbyId}`);
@@ -96,23 +90,25 @@ const NightReveal = () => {
           console.error("There was an error connecting or subscribing: ", error);
         }
       };
-        connectAndSubscribe();}
-        console.log("I connected?");
+      connectAndSubscribe();}
+    console.log("I connected?");
 
     if (messageReceived) {
       if (messageReceived.gameState === "DISCUSSION") { // happens after ready was sent by all
         navigate("/discussion");
-      }
-      console.log("I received a MESSAGE AGAIN!");
-        findKilledPlayer();
-        console.log("KILLED PLAYER:" + killedPlayer);
-        console.log("KILLED PLAYER USERNAME:" + killedPlayer.username);
-        console.log("KILLED PLAYER ROLE:" + killedPlayer.roleName);
-
-
+      } 
     }
 
     return () => {
+      const headers = {
+        "Content-type": "application/json"
+      };
+      const body = JSON.stringify({username, gameState});
+      try{
+        stompClient.send("/app/ready", headers, body);
+      } catch (e) {
+        console.log("Something went wrong starting the game :/");
+      }
     }
 
   }, [ready]);
@@ -123,8 +119,6 @@ const NightReveal = () => {
       if (messageReceived.gameState === "DISCUSSION") {
         navigate("/discussion");
       }
-      findKilledPlayer();
-      console.log("Killed player: " + killedPlayer);
     }
   }, [messageReceived]); 
 
@@ -133,30 +127,41 @@ const NightReveal = () => {
   }
 
   let content = <Spinner />;
-  if(killedPlayer !== null) {
+  if(messageReceived !== null) {
+    findKilledPlayer();
     content = (
-    <div className = "rolereveal highlight">
-      {killedPlayer.username}, a {killedPlayer.roleName} was killed!        
-    </div>
+      <div className = "nightAction highlight">
+        {killedPlayer.username}, a {killedPlayer.roleName} was killed!        
+      </div>
     );
   }
 
-  //        <div className= "roleReveal highlight" >
-  //{killedPlayer.username}, a {killedPlayer.roleName} was killed!
-  //</div>
-
   return (
     <BaseContainer>
-      <div className= "roleReveal header1">Something happened during the night...
-      </div>
-      <div className= "roleReveal container">
-        {content}
-        <Button
-          width="100%"
-          height="40px"
-          onClick={()=> doSendReady()}
-        >Ok, got it!
-        </Button> 
+      <div className="nightReveal container">
+        <div className="nightReveal header">Something happened during the night...</div>
+        {(() => {
+          if (!ready) {
+            return (
+              <div className="nightReveal container">
+                {content}
+                <Button
+                  width="100%"
+                  height="40px"
+                  onClick={() => doSendReady()}
+                >
+                  Ok, got it!
+                </Button>
+              </div>
+            );
+          } else {
+            return (
+              <div className="nightReveal header2">
+                Waiting for all players to acknowledge the death of {killedPlayer.username}
+              </div>
+            );
+          }
+        })()}
       </div>
     </BaseContainer>
   );
