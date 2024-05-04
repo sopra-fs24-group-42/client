ba7007aeac9cc4557b7a2a9dfec7f2e04e4329fd
DaKnechtCoder <116026963+DaKnechtCoder@users.noreply.github.com>
2024-05-04 18:58:51 +0200

    fixing indents
    fixed indents

--- src/helpers/TextSamplesRevealNightpre.js
+++ src/helpers/TextSamplesRevealNightpre.js
@@ -1,8 +1,6 @@
 // Defines an array of possible Textx for the Narration phase
 const TextSamplesRevealNightpre = [
   "Night pre 1",
-  "Night pre 2",
-    
-  ];
+  "Night pre 2" ];
     
 export default TextSamplesRevealNightpre;
