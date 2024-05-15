3ee4523750cc598afc1b9105052c330cef7ddfcd
kuppolina <polina.kuptsova@uzh.com>
2024-05-15 18:36:15 +0200

    fixed leaderboard issue with the button and displaying the error message if no players are in the game;

--- src/components/views/Leaderboard.tsx
+++ src/components/views/Leaderboard.tsx
@@ -6,19 +6,17 @@ import { Button } from "components/ui/Button";
 import "styles/views/Leaderboard.scss";
 import BaseContainer from "components/ui/BaseContainer";
 import { Table } from "@mantine/core";
-import { Spinner } from "components/ui/Spinner";
 
 
 const Leaderboard = () => {
   const navigate = useNavigate();
   const [leaderboardTable, setLeaderboardTable] = useState(null);
   const [noPlayers, setNoPlayers] = useState(false);
-  const maxNumberOfTopPlayers = 5;
+  const maxNumberOfTopPlayers = 25;
 
   const doGetTopPlayers = async () => {
     try {
       const response = await api.get(`/leaderboards/${maxNumberOfTopPlayers}`);
-      console.log("response data" + response.data)
       setLeaderboardTable(response.data);
     } catch (error) {
       setNoPlayers(true);
@@ -36,41 +34,48 @@ const Leaderboard = () => {
     doGetTopPlayers();
   }, []);
 
-  let content = <Spinner />;
+  let content = "";
+
+  if (noPlayers === true) {
+    content = (
+      <div>
+        <h3 className="leaderboard h3">No players have played this game so far.</h3>
+      </div>
+    )
+  }
+
 
   if (leaderboardTable) {
-
-    if (noPlayers) {
-      content = <h3 className="leaderboard h3">No players have played this game so far.</h3>;
-    } else {
-      const rows = leaderboardTable.map((player) => (
-        <Table.Tr key={player.username}>
-          <Table.Td className="leaderboard table-header-position">{player.position}</Table.Td>
-          <Table.Td className="leaderboard table-header-player">{player.username.slice(0, -5)}</Table.Td>
-          <Table.Td className="leaderboard table-header-werewolves-wins">{player.numberOfWerewolfWins}</Table.Td>
-          <Table.Td className="leaderboard table-header-villagers-wins">{player.numberOfVillagerWins}</Table.Td>
-          <Table.Td className="leaderboard table-header-total-wins">{player.numberOfWins}</Table.Td>
-        </Table.Tr>
-      ));
-      content = (
+    const rows = leaderboardTable.map((player) => (
+      <Table.Tr key={player.username}>
+        <Table.Td className="leaderboard table-header-position">{player.position}</Table.Td>
+        <Table.Td className="leaderboard table-header-player">{player.username.slice(0, -5)}</Table.Td>
+        <Table.Td className="leaderboard table-header-werewolves-wins">{player.numberOfWerewolfWins}</Table.Td>
+        <Table.Td className="leaderboard table-header-villagers-wins">{player.numberOfVillagerWins}</Table.Td>
+        <Table.Td className="leaderboard table-header-total-wins">{player.numberOfWins}</Table.Td>
+      </Table.Tr>
+    ));
+    content = (
+      <div>
+        <h4 className="leaderboard h4">Players with most wins</h4>
         <Table.ScrollContainer minWidth={500}>
           <Table className="leaderboard table" horizontalSpacing="xl">
             <Table.Thead>
               <Table.Tr>
                 <Table.Th className="leaderboard table-header-position">#</Table.Th>
                 <Table.Th className="leaderboard table-header-player">Player</Table.Th>
-                <Table.Th className="leaderboard table-header-werewolves-wins">🐺 Wins </Table.Th>
-                <Table.Th className="leaderboard table-header-villagers-wins">🧑‍🌾👩‍ Wins </Table.Th>
-                <Table.Th className="leaderboard table-header-total-wins">Total Wins</Table.Th>
+                <Table.Th className="leaderboard table-header-werewolves-wins">🐺</Table.Th>
+                <Table.Th className="leaderboard table-header-villagers-wins">🧑‍🌾👩‍</Table.Th>
+                <Table.Th className="leaderboard table-header-total-wins">Total</Table.Th>
               </Table.Tr>
             </Table.Thead>
             <Table.Tbody>{rows}</Table.Tbody>
           </Table>
         </Table.ScrollContainer>
-      )
-    }
+      </div>
+    )
   }
-  
+
   return (
     <BaseContainer>
       <div className="leaderboard background-container">
